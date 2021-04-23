64cde2ff974d6c937686aebbd7bee57761ca763e
yu.deng <yu.deng@capstonegaming.com>
2021-04-23 13:53:59 +0800

    材质球动态加载贴图

---
+++ Runtime/DynamicImage/DynamicMaterialTexture.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Capstones.UnityEngineEx
+{
+    [ExecuteInEditMode]
+    public class DynamicMaterialTexture : MonoBehaviour
+    {
+        public string[] MatProps;
+        public string[] Paths;
+        bool Loaded;
+
+        void OnEnable()
+        {
+            if (!Loaded)
+            {
+                Loaded = true;
+                LoadMaterialTexture();
+            }
+        }
+
+        private void LoadMaterialTexture()
+        {
+            var mesh = GetComponent<MeshRenderer>();
+            if (mesh == null)
+                return;
+
+            for (int i = 0; i < Paths.Length; i++)
+            {
+                var path = Paths[i];
+                var tex = ResManager.LoadRes<Texture>(path);
+                if (tex == null)
+                    continue;
+
+                var prop = new MaterialPropertyBlock();
+                prop.SetTexture(MatProps[i], tex);
+                mesh.SetPropertyBlock(prop);
+            }
+        }
+    }
+}
