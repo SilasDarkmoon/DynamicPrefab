d0e4c114fa9fa7a8f00d6ee0cfb1ed9987f76061
WangXiong <xiong.wang@capstonegaming.com>
2021-04-27 18:29:35 +0800

    Modify: Mat动态修改属性

--- Runtime/DynamicImage/DynamicMaterialTexture.cs
+++ Runtime/DynamicImage/DynamicMaterialTexture.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Capstones.UnityEngineEx
 {
@@ -8,31 +9,65 @@ namespace Capstones.UnityEngineEx
         public string[] Paths;
         bool Loaded;
 
-        void OnEnable()
+        private void Start()
         {
-            if (!Loaded)
-            {
-                Loaded = true;
-                LoadMaterialTexture();
-            }
+            //StartCoroutine(Delay());
         }
 
-        private void LoadMaterialTexture()
+        /// <summary>
+        /// 在加载场景后需要延迟加载，原因未知
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator Delay()
         {
-            var mesh = GetComponent<MeshRenderer>();
-            if (mesh == null)
-                return;
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            LoadMaterialTexture();
+        }
 
-            for (int i = 0; i < Paths.Length; i++)
+        void OnEnable()
+        {
+            //if (!Loaded)
+            //{
+            //    Loaded = true;
+            //    LoadMaterialTexture();
+            //}
+        }
+
+        public void LoadMaterialTexture()
+        {
+            var meshs = GetComponents<MeshRenderer>();
+            foreach (var mesh in meshs)
             {
-                var path = Paths[i];
-                var tex = ResManager.LoadRes<Texture>(path);
-                if (tex == null)
+                if (mesh == null)
+                {
+                    Debug.LogError("DynamicMaterialTexture::mesh is null. " + gameObject.name);
                     continue;
+                }
+                if (mesh.sharedMaterials.Length != 1)
+                {
+                    Debug.LogError("DynamicMaterialTexture::sharedMaterials is null. " + gameObject.name);
+                    continue;
+                }
 
-                var prop = new MaterialPropertyBlock();
-                prop.SetTexture(MatProps[i], tex);
-                mesh.SetPropertyBlock(prop);
+                for (int i = 0; i < Paths.Length; i++)
+                {
+                    var path = Paths[i];
+                    var tex = ResManager.LoadRes<Texture>(path);
+                    if (tex == null)
+                    {
+                        Debug.LogError("DynamicMaterialTexture::tex is null. " + path);
+                        continue;
+                    }
+
+                    //var prop = new MaterialPropertyBlock();
+                    //mesh.GetPropertyBlock(prop);
+                    //prop.SetTexture(MatProps[i], tex);
+                    //mesh.SetPropertyBlock(prop);
+
+                    //TODO: 临时方案，因为球场加载会出莫名其妙的Bug
+                    mesh.sharedMaterial.SetTexture(MatProps[i], tex);
+                }
             }
         }
     }
